bce0009f281fa3dcc85219e7fec0bf74794490be
ridwanalfarezi <donnysutiandy@gmail.com>
2026-01-13 22:49:47 +0700

    fix: revert PORT configuration to default value of 3000

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import path from "path";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const NODE_ENV = process.env.NODE_ENV || "development";
 
 /**
